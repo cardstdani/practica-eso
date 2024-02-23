e920780d873fa4de6d9d81e1700068e2a33d83cd
Daniel García Solla <danielgarciasolla0@gmail.com>
2024-02-23 20:14:41 +0100

    ¡¡PERO ESTO QUÉ ES!!😡

--- UVash.c
+++ UVash.c
@@ -13,7 +13,8 @@ struct command {
   char **arg_array;
   char *path;
 };
-
+enum errorType { ERROR, NO_ERROR };
+const struct command nullCommand = {0, NULL, NULL};
 const char error_message[30] = "An error has occurred\n";
 
 void printError() {
@@ -21,13 +22,31 @@ void printError() {
   fflush(stderr);
 }
 
+void cleanCommand(struct command *cmd) {
+  free(cmd->arg_array);
+  cmd->num_argumentos = 0;
+  cmd->path = NULL;
+}
+
+void freeMemory(char *buff, char **comandos_paralelos, struct command *comandos[], size_t num_parallel_commands) {
+  free(comandos_paralelos);
+  buff++;
+  for (size_t i = 0; i < num_parallel_commands; i++) {
+    for (struct command *cmd = comandos[i]; cmd->num_argumentos != 0; cmd++)
+      cleanCommand(cmd);
+    free(comandos[i]);
+  }
+}
+
 struct command separarComando(char *buff) {
   struct command comando = (struct command){0, (char **)calloc(MAX_ARGUMENTS, sizeof(char *)), NULL};
   char *ptr = buff;
   char *path_ptr = buff;
   while ((path_ptr = strsep(&buff, ">")) != NULL) { // Separar redireccion
-    if (comando.num_argumentos > 1 || *path_ptr == '\0')
-      printError();
+    if (comando.num_argumentos > 1 || *path_ptr == '\0') {
+      cleanCommand(&comando);
+      return nullCommand;
+    }
     if (comando.num_argumentos++ == 1)
       comando.path = path_ptr;
   }
@@ -38,107 +57,156 @@ struct command separarComando(char *buff) {
       continue;
     if (comando.num_argumentos < MAX_ARGUMENTS)
       comando.arg_array[comando.num_argumentos++] = ptr;
-    else
-      printError();
+    else {
+      cleanCommand(&comando);
+      return nullCommand;
+    }
   }
-  // Resizear el array de argumentos
-  comando.arg_array = (char **)realloc(comando.arg_array, comando.num_argumentos * sizeof(char *));
-  if (comando.arg_array == NULL) {
-    printError();
-    exit(1);
+
+  // Comprobar si el comando separado es valido y Resizear el array de argumentos
+  if (comando.num_argumentos == 0 || (comando.arg_array = (char **)realloc(comando.arg_array, comando.num_argumentos * sizeof(char *))) == NULL) {
+    cleanCommand(&comando);
+    return nullCommand;
   }
   return comando;
 }
 
+// separar entrada por caracter separador y limite
 char **separarString(char *buff, char *delimiter, size_t *size, size_t limit) {
-  // separar entrada por caracter separador y limite
   char **comandos = (char **)calloc(limit, sizeof(char *));
+  if (comandos == NULL) {
+    free(comandos);
+    return NULL;
+  }
   char *ptr = buff;
-  size_t num_comandos = 0;
+  *size = 0; // Contador de comandos
   while ((ptr = strsep(&buff, delimiter)) != NULL) {
     if (*ptr == '\0')
       continue;
-    if (num_comandos < limit)
-      comandos[num_comandos++] = ptr;
-    else
-      printError();
+    if (*size < limit)
+      comandos[(*size)++] = ptr;
+    else {
+      free(comandos);
+      return NULL;
+    }
   }
   // Resizear el array de comandos
-  comandos = (char **)realloc(comandos, num_comandos * sizeof(char *));
+  comandos = (char **)realloc(comandos, (*size) * sizeof(char *));
   if (comandos == NULL) {
-    printError();
-    exit(1);
+    free(comandos);
+    return NULL;
   }
-  *size = num_comandos;
   return comandos;
 }
 
-void printCommand(struct command *cmd) {
-  printf("Number of arguments: %zu\nPath:%s\n", cmd->num_argumentos, cmd->path);
-  for (size_t i = 0; i < cmd->num_argumentos; i++)
-    printf("[%zu]: %s\n", i, cmd->arg_array[i]);
-}
-
-void cleanCommand(struct command *cmd) {
-  free(cmd->arg_array);
-  cmd->num_argumentos = 0;
-  cmd->path = NULL;
-}
-
-void ejecutarComando(struct command *cmd) {
-  printCommand(cmd);
+enum errorType ejecutarComando(struct command *cmd) {
   // Comprobar si el usuario quiere salir del shell (obviamente no va a querer, el shell es demasiado bueno)
   if (strcmp(cmd->arg_array[0], "exit") == 0) {
     if (cmd->num_argumentos <= 1)
       exit(0);
     else
-      printError();
+      return ERROR;
   }
 
   // Comprobar si el usuario pone un cd
-  if (strcmp(cmd->arg_array[0], "cd") == 0 && (cmd->num_argumentos != 2 || chdir(cmd->arg_array[1]) != 0))
-    printError();
+  if (strcmp(cmd->arg_array[0], "cd") == 0)
+    return (cmd->num_argumentos != 2 || chdir(cmd->arg_array[1]) != 0) ? ERROR : NO_ERROR;
+
+  // Ejecutar comando
+  int estado;
+  pid_t pid = fork();
+  if (pid == 0) {
+    estado = execvp(cmd->arg_array[0], cmd->arg_array);
+    if (estado == -1)
+      return ERROR;
+  } else if (pid > 0) {
+    wait(NULL);
+  } else
+    return ERROR;
   cleanCommand(cmd);
+  return NO_ERROR;
+}
+
+enum errorType procesarEntrada(char *buff) { // Que mal queda el tipo enum noseque, pero es facil de entender (despues)
+  // Separar comandos paralelos
+  size_t num_parallel_commands = 0;
+  char **comandos_paralelos = separarString(buff, "&\n", &num_parallel_commands, MAX_PARALLEL_COMMANDS);
+  if (comandos_paralelos == NULL)
+    return ERROR;
+
+  // Construir tensor de comandos
+  struct command *comandos[num_parallel_commands];
+  for (size_t i = 0; i < num_parallel_commands; i++) {
+    size_t num_comandos_internos = 0;
+    char **comandos_internos = separarString(comandos_paralelos[i], ";\n", &num_comandos_internos, MAX_COMMANDS);
+    // Construir comandos a partir de secuencia
+    comandos[i] = (struct command *)calloc(num_comandos_internos + 1, sizeof(struct command));
+    for (size_t j = 0; j < num_comandos_internos; j++) {
+      comandos[i][j] = separarComando(comandos_internos[j]);
+      if (comandos[i][j].num_argumentos == 0) { // Error al separar comando
+        free(comandos_internos);
+        freeMemory(buff, comandos_paralelos, comandos, i + 1);
+        return ERROR;
+      }
+    }
+    free(comandos_internos);
+  }
+
+  // Ejecutar comandos
+  for (size_t i = 0; i < num_parallel_commands; i++)
+    for (struct command *cmd = comandos[i]; cmd->num_argumentos != 0; cmd++)
+      if (ejecutarComando(cmd) == ERROR) {
+        freeMemory(buff, comandos_paralelos, comandos, num_parallel_commands);
+        return ERROR;
+      }
+  for (size_t i = 0; i < num_parallel_commands; i++)
+    wait(NULL);
+  freeMemory(buff, comandos_paralelos, comandos, num_parallel_commands);
+  return NO_ERROR;
 }
 
 int main(int argc, char **args) {
-  while (1) {
-    fprintf(stdout, "UVash> ");
-    // Lectura de comando de entrada
+  if (argc == 2) { // Modo bash
+    FILE *fp = fopen(args[1], "r");
+    if (fp == NULL) {
+      printError();
+      exit(1);
+    }
+
     char *buff = NULL;
     size_t input_size = 0;
-    if (getline(&buff, &input_size, stdin) == -1) {
-      printError();
-      continue;
-    }
-    if (buff[0] == '\n') { // Continuar si no hay nada escrito
-      free(buff);
-      continue;
-    }
+    while (getline(&buff, &input_size, fp) != -1) {
+      if (buff[0] == '\n') { // Continuar si no hay nada escrito
+        free(buff);
+        continue;
+      }
 
-    // Separar comandos paralelos
-    size_t num_parallel_commands = 0;
-    char **comandos_paralelos = separarString(buff, "&\n", &num_parallel_commands, MAX_PARALLEL_COMMANDS);
-    struct command *comandos[num_parallel_commands];
-
-    // Construir tensor de comandos
-    for (size_t i = 0; i < num_parallel_commands; i++) {
-      size_t num_comandos_internos = 0;
-      char **comandos_internos = separarString(comandos_paralelos[i], ";\n", &num_comandos_internos, MAX_COMMANDS);
-      comandos[i] = (struct command *)calloc(num_comandos_internos + 1, sizeof(struct command));
-      for (size_t j = 0; j < num_comandos_internos; j++)
-        comandos[i][j] = separarComando(comandos_internos[j]);
-      free(comandos_internos);
+      if (procesarEntrada(buff) == ERROR)
+        printError();
     }
-    free(comandos_paralelos);
+  } else if (argc == 1) { // Modo interactivo
+    while (1) {
+      fprintf(stdout, "UVash> ");
+      // Lectura de comando de entrada
+      char *buff = NULL;
+      size_t input_size = 0;
+      if (getline(&buff, &input_size, stdin) == -1) {
+        printError();
+        continue;
+      }
+      if (buff[0] == '\n') { // Continuar si no hay nada escrito
+        free(buff);
+        continue;
+      }
 
-    // Ejecutar comandos
-    for (size_t i = 0; i < num_parallel_commands; i++) {
-      for (struct command *cmd = comandos[i]; cmd->num_argumentos != 0; cmd++)
-        ejecutarComando(cmd);
-      free(comandos[i]);
+      if (procesarEntrada(buff) == ERROR) {
+        printError();
+        continue;
+      }
     }
-    free(buff);
+  } else {
+    printError();
+    exit(1);
   }
   return 0;
 }
